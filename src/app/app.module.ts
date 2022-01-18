0520c72944036e494b65fd2abf33f11b3e452d06
JCalhoun95 <JCalhoun_95@yahoo.com>
2022-01-18 15:14:15 -0600

    basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { AftercareComponent } from './aftercare/aftercare.component';
+import { InstagramComponent } from './instagram/instagram.component';
+import { ArtComponent } from './art/art.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PoliciesComponent,
+    AftercareComponent,
+    InstagramComponent,
+    ArtComponent
   ],
   imports: [
     BrowserModule,
