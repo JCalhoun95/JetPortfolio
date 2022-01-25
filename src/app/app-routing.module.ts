4767a4b284e96ce6b32fa2168d4ec137fe51fbff
JCalhoun95 <JCalhoun_95@yahoo.com>
2022-01-24 19:01:29 -0600

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AftercareComponent } from './aftercare/aftercare.component';
+import { ArtComponent } from './art/art.component';
+import { HomeComponent } from './home/home.component';
+import { PoliciesComponent } from './policies/policies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"art", component:ArtComponent},
+  {path:"pollicies", component:PoliciesComponent},
+  {path:"aftercare", component:AftercareComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
